22b55f01679b6ee6c60340c00a7a7cc4fa6a2459
daniel94563 <daniel94564@gmail.com>
2021-01-15 00:28:38 +0800

    upload data preprocess
    update mapping fuction

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+  </v-container>
+</template>
+
+<script>
+
+   var myData =[{
+  "name": "Ian Devling",
+  "imageUrl": "https://raw.githubusercontent.com/bumbeishvili/Ass…s/master/Projects/D3/Organization%20Chart/cto.jpg",
+  "id": "O-1",
+  "parentId": null
+},
+{
+  "name": "Ian Devling",
+  "imageUrl": "https://raw.githubusercontent.com/bumbeishvili/Ass…s/master/Projects/D3/Organization%20Chart/cto.jpg",
+  "id": "O-2",
+  "parentId": "O-1"
+}];
+var data = myData.map(d=>{
+  return {
+    'nodeId':d.id,
+    "parentNodeId":d.parentId
+  }
+
+
+});
+
+console.log(myData);
+console.log(myData[1]);
+console.log(data);
+  export default {
+    name: 'HelloWorld',
+
+
+  }
+</script>
